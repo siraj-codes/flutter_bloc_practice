fa111fe2e8700df5a5c02c393879b9e05beac8e5
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-10 19:54:39 +0500

    home_page2 created

---
+++ lib/screens/task2/home2_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home2Page extends StatelessWidget {
+  const Home2Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
