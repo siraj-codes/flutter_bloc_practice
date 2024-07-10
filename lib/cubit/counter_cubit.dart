e62ff8cd9a2ca8ef7015d155be41d806fd083788
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-10 17:21:30 +0500

    basic_counter_app completed

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,5 +1,17 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CounterCubit extends Cubit {
+class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0);
+
+  void inc() {
+    emit(state + 1);
+  }
+
+  void dec() {
+    if (state > 0) emit(state - 1);
+  }
+
+  void reset() {
+    emit(state - state);
+  }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_practice/cubit/counter_cubit.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,27 +10,67 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final count = CounterCubit().state;
+  final counterCubit = CounterCubit();
+
+  @override
+  void dispose() {
+    counterCubit.close(); // Don't forget to close the cubit
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        title: const Text('Counter App'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'Count is : $count',
-          style: const TextStyle(fontSize: 27),
-        ),
-      ),
-      floatingActionButton: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.add,
-            color: Colors.blueGrey,
-          )),
-    );
+    return BlocBuilder<CounterCubit, int>(
+        bloc: counterCubit,
+        builder: (context, counter) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.blueGrey,
+              title: const Text('Counter App'),
+              centerTitle: true,
+            ),
+            body: Center(
+              child: Text(
+                'Count is : $counter',
+                style: const TextStyle(fontSize: 27),
+              ),
+            ),
+            floatingActionButton: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    counterCubit.inc();
+                  },
+                  icon: const Icon(
+                    Icons.add,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                IconButton(
+                  onPressed: () {
+                    counterCubit.dec();
+                  },
+                  icon: const Icon(
+                    Icons.minimize,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                  onPressed: () {
+                    counterCubit.reset();
+                  },
+                  child: const Text('Reset'),
+                )
+              ],
+            ),
+          );
+        });
   }
 }
