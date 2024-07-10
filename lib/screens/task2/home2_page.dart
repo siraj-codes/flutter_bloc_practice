4e27e551997cd66474a78f83dd91b666a87db042
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-10 19:57:21 +0500

    basic layout seted

--- lib/screens/task2/home2_page.dart
+++ lib/screens/task2/home2_page.dart
@@ -1,10 +1,85 @@
 import 'package:flutter/material.dart';
 
 class Home2Page extends StatelessWidget {
-  const Home2Page({Key? key}) : super(key: key);
+  const Home2Page({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text('Counter App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Count is : ',
+              style: const TextStyle(fontSize: 27),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const Test2Page();
+                    },
+                  ),
+                );
+              },
+              child: Container(
+                height: 50,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Next Page',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.add,
+              color: Colors.blueGrey,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.minimize,
+              color: Colors.blueGrey,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Reset'),
+          )
+        ],
+      ),
+    );
   }
 }
