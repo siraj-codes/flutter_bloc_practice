a119c19dc4e51c5da8fad6529406f2ad31b055c5
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-10 20:17:51 +0500

    CounterIncremented event created

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -2,6 +2,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterIncremented {}
 
+class CounterDecremented {}
+
 class CounterBloc extends Bloc<CounterIncremented, int> {
-  CounterBloc() : super(0);
+  CounterBloc() : super(0) {
+    on<CounterIncremented>((event, emit) {
+      emit(state + 1);
+    });
+  }
 }
