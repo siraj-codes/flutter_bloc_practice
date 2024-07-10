af8d202ab98723c21219777238f8b3904de70573
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-10 20:03:17 +0500

    body for test 2 page

--- lib/screens/task2/test2.dart
+++ lib/screens/task2/test2.dart
@@ -6,11 +6,40 @@ class Test2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Test 2 page'),
-        backgroundColor: Colors.blueGrey,
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Test 2 page'),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add,
+                color: Colors.blueGrey,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.minimize,
+                color: Colors.blueGrey,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Reset'),
+            )
+          ],
+        ));
   }
 }
