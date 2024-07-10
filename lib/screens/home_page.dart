e2a267e10aa93e447bb41253764515a190b11247
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-10 16:52:50 +0500

    basic layout created

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_practice/cubit/counter_cubit.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,14 +9,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final count = CounterCubit().state;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
         title: const Text('Counter App'),
         centerTitle: true,
       ),
+      body: Center(
+        child: Text(
+          'Count is : $count',
+          style: const TextStyle(fontSize: 27),
+        ),
+      ),
+      floatingActionButton: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.add,
+            color: Colors.blueGrey,
+          )),
     );
   }
 }
