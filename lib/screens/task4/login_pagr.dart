b6ad40e1dd85676c7fd052944300c1a5412a6554
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-11 15:02:54 +0500

    basic ui  changes

--- lib/screens/task4/login_pagr.dart
+++ lib/screens/task4/login_pagr.dart
@@ -5,6 +5,64 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final TextEditingController eC = TextEditingController();
+    final TextEditingController pC = TextEditingController();
+
+    return Scaffold(
+      backgroundColor: Colors.red.shade200,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Sign Up'),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(35),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: eC,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                filled: true,
+                fillColor: Colors.lightBlue[100],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: pC,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                filled: true,
+                fillColor: Colors.lightBlue[100],
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 55,
+              width: 275,
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: const Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
