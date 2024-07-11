fbc39a87e7fdf76113ae9ff66239a7157b5b2ff0
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-11 13:30:47 +0500

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_practice/bloc/counter_bloc.dart';
-import 'package:flutter_bloc_practice/cubit/counter_cubit.dart';
+// import 'package:flutter_bloc_practice/bloc/counter_bloc.dart';
+// import 'package:flutter_bloc_practice/cubit/counter_cubit.dart';
 // import 'package:flutter_bloc_practice/cubit/counter_cubit.dart';
 // import 'package:flutter_bloc_practice/screens/task1/home_page.dart';
 // import 'package:flutter_bloc_practice/screens/task2/home2_page.dart';
