72c684627c4d9c10fea45e93b6b7e65c15d96e55
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-11 13:36:40 +0500

    todo_list body created

--- lib/screens/task3/todo_list.dart
+++ lib/screens/task3/todo_list.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_practice/screens/task3/cubit/todo_cubit.dart';
+import 'package:flutter_bloc_practice/screens/task3/models/todo_model.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList({super.key});
@@ -9,6 +12,20 @@ class TodoList extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Todo List'),
+      ),
+      body: BlocBuilder<TodoCubit, List<Todo>>(
+        builder: (context, todos) {
+          return ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              final todo = todos[index];
+
+              return ListTile(
+                title: Text(todo.name),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
